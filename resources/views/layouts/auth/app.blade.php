6c2076def944b38e51cc67bed7ca3ce7ac186526
Nesta Maulana <nesta.maulana@sangkuriang.co.id>
2023-11-15 15:17:25 +0700

    hotfix(login): Fixing see password button

--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -3,71 +3,89 @@
 
 <head>
 
-  <meta charset="utf-8" />
-  <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-  <meta content="NM" name="author" />
-  <!-- App favicon -->
-  <link rel="shortcut icon" href="{{ asset('assets/images/logo/logo-sm.png') }}">
+    <meta charset="utf-8" />
+    <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="NM" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/logo-sm.png') }}">
 
-  <!-- Layout config Js -->
-  <script src="{{ asset('assets/js/layout.js') }}"></script>
-  <!-- Bootstrap Css -->
-  <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-  <!-- Icons Css -->
-  <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-  <!-- App Css-->
-  <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
-  <!-- custom Css-->
-  <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Layout config Js -->
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
 
 
 </head>
 
 <body>
 
-  <div class="auth-page-wrapper pt-5">
+    <div class="auth-page-wrapper pt-5">
 
-    <!-- auth page content -->
-    <div class="auth-page-content mt-5">
-      <div class="container">
-        <!-- content -->
-        @yield('content')
-        <!-- end content -->
-      </div>
-      <!-- end container -->
-    </div>
-    <!-- end auth page content -->
-
-    <!-- footer -->
-    <footer class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-center">
-              <p class="mb-0 text-muted">&copy;
-                <script>
-                  document.write(new Date().getFullYear())
-                </script> Library Managament System. Crafted with <i class="mdi mdi-heart text-danger"></i>
-              </p>
+        <!-- auth page content -->
+        <div class="auth-page-content mt-5">
+            <div class="container">
+                <!-- content -->
+                @yield('content')
+                <!-- end content -->
             </div>
-          </div>
+            <!-- end container -->
         </div>
-      </div>
-    </footer>
-    <!-- end Footer -->
-  </div>
-  <!-- end auth-page-wrapper -->
+        <!-- end auth page content -->
 
-  <!-- JAVASCRIPT -->
-  <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-  <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
-  <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
-  <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
-  <script src="{{ asset('assets/js/plugins.js') }}"></script>
+        <!-- footer -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0 text-muted">&copy;
+                                <script>
+                                    document.write(new Date().getFullYear())
+                                </script> Library Managament System. Crafted with <i
+                                    class="mdi mdi-heart text-danger"></i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer -->
+    </div>
+    <!-- end auth-page-wrapper -->
 
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#password-addon').click(function() {
+                // Temukan elemen input password
+                var passwordInput = $('#password-input');
+
+                // Ubah tipe input dari password ke text atau sebaliknya
+                var inputType = passwordInput.attr('type') === 'password' ? 'text' : 'password';
+                passwordInput.attr('type', inputType);
+
+                // Ubah ikon mata sesuai dengan tipe input
+                var eyeIcon = $('#password-addon i');
+                eyeIcon.toggleClass('ri-eye-fill ri-eye-off-fill');
+            });
+        });
+    </script>
 </body>
 
 </html>
